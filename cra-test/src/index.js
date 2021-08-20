2d25f9a50ef3c56aedc213f2faaa5458168e2292
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-08-20 17:59:01 +0900

    react-with-cra

---
+++ cra-test/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+//process.env.{변수이름}
+//process.env.NODE_ENV
+//"npm start" => development
+//"npm test" => test
+//"npm run build" => production
+console.log("process.env.NODE_ENV : ", process.env.NODE_ENV);
+console.log("REACT_APP_API_URL : ", process.env.REACT_APP_API_URL);
+console.log("REACT_APP_TEMP1: ", process.env.REACT_APP_TEMP);
+// REACT_APP_*
+// set REACT_APP_API_URL=api.myapp.com npm start
